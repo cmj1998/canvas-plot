40f743649110ba25e98dfaa0be74825221719d6d
放弃 <5133333+chenmingjie123@user.noreply.gitee.com>
2019-08-06 11:45:30 +0800

    cmj:19/8/6 方形bug解决,橡皮擦基础完成,代办(直线)

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@
           :style="{ 'border-color' : controller==3? '#fff':'#000'}"
         ></i>
       </el-button>
+      <!-- 直线 -->
       <el-button plain @click="controller = 4" title="直线" :class="[ controller==4?'pitch-btn':'' ]">
         <i
           class="btn-icon"
@@ -30,9 +31,16 @@
         ></i>
       </el-button>
     </div>
-    <div id="max-box">
+    <div id="max-box" :style="{'cursor':isCursor?'default':'none'}">
       <!-- 画布 -->
-      <canvas id="myCanvas" width="1300" height="800" @mousedown="canvas($event)"></canvas>
+      <canvas
+        id="myCanvas"
+        width="1300"
+        height="800"
+        @mousedown="canvas($event)"
+        @mouseenter="controller == 10 ? isShowEraser = true :isShowEraser = false"
+        @mouseleave="isShowEraser = false"
+      ></canvas>
       <!-- 清空画布按钮 -->
       <el-button
         type="primary"
@@ -49,6 +57,7 @@
         @click="eraser($event)"
         title="橡皮擦"
       ></el-button>
+      <div id="eraser__class" v-show="isShowEraser"></div>
       <!-- 颜色选择 -->
       <div class="btn-col">
         <el-color-picker v-model="color1" title="颜色选择"></el-color-picker>
@@ -64,6 +73,9 @@ export default {
       c: null, //画布
       ctx: null, //画布的getContext("2d")
       maxBox: null, //包裹画布的盒子
+      eraserClass: null, //橡皮擦样式
+      isCursor: true, //是否显示鼠标
+      isShowEraser: false, //显示橡皮擦
       color1: "#000", //颜色选择器
       controller: 1 //功能控制器
     };
@@ -74,6 +86,7 @@ export default {
       this.c = document.getElementById("myCanvas");
       this.ctx = this.c.getContext("2d");
       this.maxBox = document.getElementById("max-box");
+      this.eraserClass = document.getElementById("eraser__class");
     },
     //画布功能  1=画笔 2=方块
     canvas(eve) {
@@ -92,7 +105,7 @@ export default {
           this.solid(eve);
       }
     },
-    //画笔
+    //画笔 -1
     brush(eve) {
       this.ctx.beginPath(); //重置路径
       this.ctx.moveTo(eve.layerX, eve.layerY); //定点
@@ -106,7 +119,7 @@ export default {
         document.onmousemove = null; //清除移动事件
       };
     },
-    //方块
+    //方块 -2
     diamonds(eve) {
       this.ctx.beginPath(); //重置路径
       let _this = this;
@@ -131,9 +144,10 @@ export default {
           div.style.height = h + "px";
         } else {
           //左上画方块
-          console.log("负数");
-          div.style.width = 10 + "px";
-          div.style.height = 10 + "px";
+          div.style.width = Math.abs(w) + "px";
+          div.style.height = Math.abs(h) + "px";
+          div.style.left = parseInt(div.style.left) + w + "px";
+          div.style.top = parseInt(div.style.top) + h + "px";
         }
       };
       document.onmouseup = function() {
@@ -144,17 +158,44 @@ export default {
         document.onmousemove = null; //清除移动事件
       };
     },
-    //直线
+    //圆 -3
+    //直线 -4
     solid(eve) {},
-    //橡皮擦
+    //橡皮擦 -10
     eraser(eve) {
-      this.ctx.save();
-      this.ctx.beginPath();
-      //1.圆的中心的 x 坐标 2.圆的中心的 y 坐标 3.圆的半径 4.起始角，以弧度计。（弧的圆形的三点钟位置是 0 度） 5.结束角，以弧度计  6.可选。规定应该逆时针还是顺时针绘图。False = 顺时针，true = 逆时针。
-      this.ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-      this.ctx.clip();
-      this.ctx.clearRect(0, 0, this.c.width, this.c.height);
-      this.ctx.restore();
+      // this.ctx.save();
+      // this.ctx.beginPath();
+      // //1.圆的中心的 x 坐标 2.圆的中心的 y 坐标 3.圆的半径 4.起始角，以弧度计。（弧的圆形的三点钟位置是 0 度） 5.结束角，以弧度计  6.可选。规定应该逆时针还是顺时针绘图。False = 顺时针，true = 逆时针。
+      // this.ctx.arc(50, 50, 50, 0, 2 * Math.PI);
+      // this.ctx.clip();
+      // this.ctx.clearRect(0, 0, this.c.width, this.c.height);
+      // this.ctx.restore();
+      this.isCursor = !this.isCursor;
+      this.controller == 10 ? (this.controller = 1) : (this.controller = 10);
+      this.isShowEraser = !this.isShowEraser;
+      var _this = this;
+      this.maxBox.onmousemove = function(eve) {
+        _this.eraserClass.style.left =
+          eve.clientX - _this.maxBox.offsetLeft + "px";
+        _this.eraserClass.style.top =
+          eve.clientY - _this.maxBox.offsetTop + "px";
+      };
+      this.c.onmousedown = function() {
+        _this.c.onmousemove = function(eve) {
+          _this.ctx.save();
+          _this.ctx.beginPath();
+          //1.圆的中心的 x 坐标 2.圆的中心的 y 坐标 3.圆的半径 4.起始角，以弧度计。（弧的圆形的三点钟位置是 0 度） 5.结束角，以弧度计  6.可选。规定应该逆时针还是顺时针绘图。False = 顺时针，true = 逆时针。
+          _this.ctx.arc(eve.clientX - _this.maxBox.offsetLeft+10, eve.clientY - _this.maxBox.offsetTop+10, 10, 0, 2 * Math.PI);
+          _this.ctx.clip();
+          _this.ctx.clearRect(0, 0, _this.c.width, _this.c.height);
+          _this.ctx.restore();
+        };
+        _this.c.onmouseup = function() {
+          // _this.ctx.restore();
+          // _this.ctx.save();
+          _this.c.onmousemove = null;
+        };
+      };
     },
     //清空画布
     emptyCanvas() {
@@ -163,8 +204,32 @@ export default {
       document.onmouseup = null; //解决点击清除按钮后切换功能(画笔,方块...),会显示清除前最后一个图像的BUG
     }
   },
+  watch: {
+    //监听功能控制的值,不为橡皮擦时(10),显示鼠标,隐藏橡皮擦
+    controller: function(newData, oldData) {
+      if (newData != 10) {
+        this.isCursor = true;
+        this.isShowEraser = false;
+      }
+    }
+  },
   mounted() {
     this.defCanvas();
+    let _this = this;
+    this.maxBox.onmousedown = function(eve) {
+      //鼠标右键的点击事件
+      if (eve.which == 3) {
+        console.log("右键点击");
+        _this.isShowEraser = false;
+        _this.isCursor = true;
+        _this.controller = 1;
+        _this.c.onmousedown = null
+      }
+    };
+    //清除浏览器的默认右键点击提示
+    document.oncontextmenu = function() {
+      return false;
+    };
   }
 };
 </script>
@@ -207,27 +272,41 @@ body {
   height: 800px;
   position: relative;
   margin: 100px auto;
+  overflow: hidden;
+  border: 1px solid #000;
 }
 /* 画布 */
 #myCanvas {
-  border: 1px solid #000;
+  /* border: 1px solid #000; */
   display: inline-block;
+  overflow: hidden;
 }
 /* 清空画布 */
 .btn__del-canvas {
   position: absolute;
-  right: -1px;
-  top: 1px;
+  right: 0px;
+  top: 0px;
 }
 /* 橡皮擦 */
 .btn__eraser-canvas {
   position: absolute;
-  right: -1px;
+  right: 0px;
   top: 50px;
+}
+/* 橡皮擦的样式 */
+#eraser__class {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 1px solid #000;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
 }
 .btn-col {
   position: absolute;
   top: 0px;
-  right: -50px;
+  right: 80px;
 }
 </style>
